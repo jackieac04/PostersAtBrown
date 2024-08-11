d62f900360c8a0072b2e242991e72b95500efe54
AJ <amanda_wu@brown.edu>
2024-08-11 17:43:57 -0400

    cors config update

--- src/main/java/edu/brown/cs/student/main/CorsConfig.java
+++ src/main/java/edu/brown/cs/student/main/CorsConfig.java
@@ -15,7 +15,8 @@ public class CorsConfig {
       public void addCorsMappings(CorsRegistry registry) {
         registry
             .addMapping("/users/create") // Map specific endpoint
-            .allowedOrigins("http://localhost:5173")
+            .allowedOrigins("https://posters-at-brown.vercel.app/","https://postersatbrown.com", "http://postersatbrown.com",
+                    "www.postersatbrown.com","postersatbrown.com", "http://localhost:5173")
             .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
             .allowedHeaders("*")
             .allowCredentials(true)
@@ -23,39 +24,45 @@ public class CorsConfig {
 
         registry
             .addMapping("/users/{id}")
-            .allowedOrigins("http://localhost:5173")
+            .allowedOrigins("https://posters-at-brown.vercel.app/","https://postersatbrown.com", "http://postersatbrown.com",
+                    "www.postersatbrown.com","postersatbrown.com", "http://localhost:5173")
             .allowedMethods("GET", "POST", "PUT", "DELETE")
             .allowedHeaders("*") // Allow any headers
             .allowCredentials(true);
         registry
             .addMapping("/users/savedPosters/{id}")
-            .allowedOrigins("http://localhost:5173")
+            .allowedOrigins("https://posters-at-brown.vercel.app/","https://postersatbrown.com", "http://postersatbrown.com",
+                    "www.postersatbrown.com","postersatbrown.com", "http://localhost:5173")
             .allowedMethods("GET") // Adjust allowed methods based on your needs
             .allowedHeaders("*") // Allow any headers
             .allowCredentials(true)
             .maxAge(3600);
         registry
             .addMapping("/users/createdPosters/{id}")
-            .allowedOrigins("http://localhost:5173")
+            .allowedOrigins("https://posters-at-brown.vercel.app/","https://postersatbrown.com", "http://postersatbrown.com",
+                    "www.postersatbrown.com","postersatbrown.com", "http://localhost:5173")
             .allowedMethods("GET") // Adjust allowed methods based on your needs
             .allowedHeaders("*") // Allow any headers
             .allowCredentials(true)
             .maxAge(3600);
         registry
             .addMapping("/users/update/{id}")
-            .allowedOrigins("http://localhost:5173")
+            .allowedOrigins("https://posters-at-brown.vercel.app/","https://postersatbrown.com", "http://postersatbrown.com",
+                    "www.postersatbrown.com","postersatbrown.com", "http://localhost:5173")
             .allowedMethods("GET", "POST", "PUT", "DELETE")
             .allowedHeaders("*") // Allow any headers
             .allowCredentials(true);
         registry
             .addMapping("/posters/create/{id}")
-            .allowedOrigins("http://localhost:5173")
+            .allowedOrigins("https://posters-at-brown.vercel.app/","https://postersatbrown.com", "http://postersatbrown.com",
+                    "www.postersatbrown.com","postersatbrown.com", "http://localhost:5173")
             .allowedMethods("POST", "PUT")
             .allowedHeaders("*")
             .allowCredentials(true);
         registry
             .addMapping("/**")
-            .allowedOrigins("http://localhost:5173")
+            .allowedOrigins("https://posters-at-brown.vercel.app/","https://postersatbrown.com", "http://postersatbrown.com",
+                    "www.postersatbrown.com","postersatbrown.com", "http://localhost:5173")
             .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
             .allowedHeaders("*")
             .allowCredentials(true)
